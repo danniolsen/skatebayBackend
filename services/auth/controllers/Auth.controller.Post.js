e141fc2a2e9ed5f00dd973246d3903ff240d52a4
danniolsen <danni.r.olsen@gmail.com>
2020-02-25 19:17:50 +0100

    verify firebase auth token

---
+++ services/auth/controllers/Auth.controller.Post.js
@@ -0,0 +1,35 @@
+"use-strict";
+
+const VerifyIdToken = (admin, idToken) => {
+  let user = {
+    displayName: null,
+    email: null,
+    photo: null,
+    uid: null,
+    provider: null
+  };
+
+  try {
+    return admin
+      .auth()
+      .verifyIdToken(idToken)
+      .then(verifiedToken => {
+        // add data to user
+        user.displayName = verifiedToken.name;
+        user.email = verifiedToken.email ? verifiedToken.email : null;
+        user.photo = verifiedToken.picture;
+        user.uid = verifiedToken.uid;
+        user.provider = verifiedToken.firebase.sign_in_provider;
+        return user;
+      })
+      .catch(error => {
+        console.log("in verify catch");
+        return { code: error.code, message: error.message };
+      });
+  } catch (tokenError) {
+    console.log(" try catch, catch");
+    return { code: tokenError.code, message: tokenError.message };
+  }
+};
+
+module.exports = { VerifyIdToken };
