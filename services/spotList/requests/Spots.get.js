128ccd015b5d2ee8a8e3d566f2efeefd5526b4cf
danniolsen <danni.r.olsen@gmail.com>
2020-03-13 10:10:17 +0100

    fixed spot type to return actual value

--- services/spotList/requests/Spots.get.js
+++ services/spotList/requests/Spots.get.js
@@ -8,7 +8,7 @@
 const GetSpotList = async user => {
   const query = {
     name: "get-spotlist",
-    text: `SELECT spot_id, spot_title, spot_images, spot_type, distance, latitude, longitude, saved_id as saved
+    text: `SELECT spot_id, spot_title, spot_images, spot_type, distance, latitude, longitude, spot_created_at, saved_id as saved
     FROM (
         SELECT *,
             (6371 * acos( cos( radians($1) ) * cos( radians( spots.latitude ) )
@@ -17,7 +17,7 @@ const GetSpotList = async user => {
         FROM (
           SELECT *
           FROM spots
-          left join spot_types on spot_types.spot_type_id = spots.spot_id
+          left join spot_types on spot_types.spot_type_id = spots.spot_type_fk
           left join saved on saved.spot_fk = spots.spot_id and saved.user_fk = $4
           left join removed on spots.spot_id = removed.spot_fk and removed.user_fk = $4
             ) AS spots
